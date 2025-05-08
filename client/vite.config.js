d519ac0c51bd5414355c2dc464287365b8fc22a8
eyob j <jemijobs22@gmail.com>
2025-05-08 17:00:16 +0300

    fix: fix vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import compression from 'vite-plugin-compression'
+// import compression from 'vite-plugin-compression'
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    compression({
-      algorithm: 'gzip',
-      ext: '.gz',
-      deleteOriginFile: false,
-    }),
+    react()
+    // ,
+    // compression({
+    //   algorithm: 'gzip',
+    //   ext: '.gz',
+    //   deleteOriginFile: false,
+    // }),
   ],
-
-  // define: {
+}) 
+ // define: {
   //   'process.env': process.env
   // }
-})
