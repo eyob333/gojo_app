d8703d0a08b66af88f10fd64a53d9ac148830fda
eyob j <jemijobs22@gmail.com>
2025-05-09 13:27:27 +0300

    fix: fetch issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,11 +7,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     react()
-    // ,
+    ,
     // compression({
     //   algorithm: 'gzip',
     //   ext: '.gz',
-    //   deleteOriginFile: false,
+    //   deleteOriginFile: true,
     // }),
   ],
 }) 
