1ec4e3b6fdcf2480c2c86efba808a489945e5acb
eyob j <jemijobs22@gmail.com>
2025-05-11 23:39:20 +0300

    fix: reaestateprofile.css again

--- client/src/componets/realEstate/ProfileCards.jsx
+++ client/src/componets/realEstate/ProfileCards.jsx
@@ -1,68 +1,26 @@
-import { useEffect, useState } from "react";
-import "./ProfileCards.css"
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import "./ProfileCards.css";
 
-function ProfileCards({name, icon}){
-    const [winSize, setWinsize] = useState(window.innerWidth)
-
-    useEffect( () => {
-        function handleResize(){
-                setWinsize(window.innerWidth)
-        }
-    
-        window.addEventListener('resize', handleResize)
-        return () => window.removeEventListener('resize', handleResize);
-    },[]);
-
-    if (winSize > 800){
-    return <>
-        <div className="container-profile-card">
-            <div className="card">
-                <h2>{name} RealEstate</h2>
-                <i class="fas fa-arrow-right"></i>
-                <p>properties</p>
-                <div className="pic" style={{backgroundImage: `url(${icon})`}}></div>
-
-                <div className="social">
-                <i className="fab fa-facebook-f">s</i>
-                <i className="fab fa-twitter">s</i>
-                <i className="fab fa-instagram">s</i>
-                <i className="fab fa-github">so</i>
-                </div>
-                <Link to={name}>
-                    <button>
-                        more
-                    </button>
-                </Link>
-            </div>
-
+function ProfileCards({ name, icon }) {
+  return (
+    <div className="container-profile-card">
+      <div className="card">
+        <h2>{name} RealEstate</h2>
+        <i className="fas fa-arrow-right"></i>
+        <p>properties</p>
+        <div className="pic" style={{ backgroundImage: `url(${icon})` }}></div>
+        <div className="social">
+          <i className="fab fa-facebook-f">s</i>
+          <i className="fab fa-twitter">s</i>
+          <i className="fab fa-instagram">s</i>
+          <i className="fab fa-github">so</i>
         </div>
-    </>
-    } else{
-        return <>
-        <div className="container-profile-card">
-            <Link to={name}>
-                <div className="card">
-                    <h2>{name} RealEstate</h2>
-                    <i className="fas fa-arrow-right"></i>
-                    <p>properties</p>
-                    <div className="pic" style={{backgroundImage: `url(${icon})`}}></div>
-
-                    <div className="social">
-                    <i className="fab fa-facebook-f">s</i>
-                    <i className="fab fa-twitter">s</i>
-                    <i className="fab fa-instagram">s</i>
-                    <i className="fab fa-github">so</i>
-                    </div>
-                        <button>
-                            see
-                        </button>
-                </div> 
-            </Link>
-        </div>   
-    </>
-    }
-
+        <Link to={name}>
+          <button>more</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default ProfileCards
+export default ProfileCards;
