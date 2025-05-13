22aa1e701fa9a3db85f361cd3580e34e7cd86f6f
eyob j <jemijobs22@gmail.com>
2025-05-13 09:57:22 +0300

    fix: small bug fix

--- client/src/componets/realEstate/Profile.jsx
+++ client/src/componets/realEstate/Profile.jsx
@@ -80,7 +80,7 @@ function Profile(){
                     </ul>
                 </div>
 
-                <div className={`profile-properties-response ${error || isLoading|| da.length == 0 ? 'nogrid' : undefined}`} >
+                <div className={`profile-properties-response ${error || isLoading? 'nogrid' : undefined}`} >
 
 
 
